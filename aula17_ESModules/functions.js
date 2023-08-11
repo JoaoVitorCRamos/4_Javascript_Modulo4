ddc4507940c33a7caf55478694f0f910539ae2dd
Joao Vitor Costa <139905189+JoaoVitorCRamos@users.noreply.github.com>
2023-08-11 20:29:08 -0300

    Pequenos ajustes esteticos

--- aula17_ESModules/functions.js
+++ aula17_ESModules/functions.js
@@ -2,15 +2,15 @@ export function label(attributes) {
     const element = document.createElement('label')
     Object.assign(element, attributes)
     return element
-  }
+}
   
-  export function input(attributes) {
+export function input(attributes) {
     const element = document.createElement('input')
     Object.assign(element, attributes)
     return element
-  }
+}
   
-  export function br() {
+export function br() {
     const element = document.createElement('br')
     return element
-  }
+}
